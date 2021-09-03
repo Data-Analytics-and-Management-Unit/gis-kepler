649ebb48b849f494744b180f4a94b819fdeeecdc
Abhishek Upperwal <a.upperwal@gmail.com>
2021-09-03 16:45:34 +0530

    Added custom Mapbox style

--- components/kepler/KeplerGLComponent.js
+++ components/kepler/KeplerGLComponent.js
@@ -35,6 +35,27 @@ function KeplerGlComponent(props) {
 						height={height}
 						theme="light"
 						appName="Urban Observatory"
+						mapStyles={[
+							{
+								id: 'basic_mapbox',
+								label: 'Basic',
+								url: 'mapbox://styles/dsu-mohua/ckt48bdp30tmi17odxeew2sq8',
+								icon: `https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/-122.3391,37.7922,9.19,0,0/400x300?access_token=pk.eyJ1IjoiZHN1LW1vaHVhIiwiYSI6ImNrcnZwZmNpdTA4dzgybnJ4Z2Q0cjdnOGcifQ.4RHMCa916LMiIEuMSmIwkQ&logo=false&attribution=false`,
+								layerGroups: [
+									{
+										slug: 'label',
+										filter: ({id}) => id.match(/(?=(label|place-|poi-))/),
+										defaultVisibility: true
+									},
+									{
+										// adding this will keep the 3d building option
+										slug: '3d building',
+										filter: () => false,
+										defaultVisibility: false
+									}
+								]
+							}
+						]}						  
 						/>
 					)}
 				</AutoSizer>
